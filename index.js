2f90b0abd1a976e5ac9c83493f5b7462d70ad06b
David Govea <govea.d@gmail.com>
2018-01-16 15:06:07 -0800

    Update injection approach

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* jshint node: true */
 'use strict';
 
+const fs = require('fs');
+
 function fsValidateConfig(addonConfig) {
   if (!addonConfig['org']) {
     throw new Error("ember-full-story requires ENV['ember-full-story']['org'] to be set when enabled.");
@@ -8,25 +10,20 @@ function fsValidateConfig(addonConfig) {
 }
 
 function fsRecordingSnipppet(addonConfig) {
-  return [
-    "<script>",
-    "window['_fs_debug'] = " + addonConfig.debug + ";",
-    "window['_fs_host'] = '" + addonConfig.host + "';",
-    "window['_fs_org'] = '" + addonConfig.org + "';",
-    "window['_fs_namespace'] = '" + addonConfig.namespace + "';",
-    "(function(m,n,e,t,l,o,g,y){",
-    "  if (e in m && m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window[\"_fs_namespace\"].'); return;}",
-    "  g=m[e]=function(a,b){g.q?g.q.push([a,b]):g._api(a,b);};g.q=[];",
-    "  o=n.createElement(t);o.async=1;o.src='https://'+_fs_host+'/s/fs.js';",
-    "  y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);",
-    "  g.identify=function(i,v){g(l,{uid:i});if(v)g(l,v)};g.setUserVars=function(v){g(l,v)};",
-    "  g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};",
-    "  g.clearUserCookie=function(c,d,i){if(!c || document.cookie.match('fs_uid=[`;`]*`[`;`]*`[`;`]*`')){",
-    "  d=n.domain;while(1){n.cookie='fs_uid=;domain='+d+",
-    "  ';path=/;expires='+new Date(0).toUTCString();i=d.indexOf('.');if(i<0)break;d=d.slice(i+1)}}};",
-    "})(window,document,window['_fs_namespace'],'script','user');",
-    "</script>"
-  ].join('\n');
+  const fsSnippet = fs.readFileSync('fs-snippet.js', 'utf8');
+
+  return `
+    if (typeof Fastboot === 'undefined') {
+      window['_fs_debug'] =${addonConfig.debug};
+      window['_fs_host'] = ${addonConfig.host};
+      window['_fs_org'] = ${addonConfig.org};
+      window['_fs_namespace'] = ${addonConfig.namespace};
+
+      var _fs_inject_script = function() { ${fsSnippet}; _fs_inject_script = null; };
+
+      ${ invokeFs ? '_fs_inject_script();' : '' }
+    }
+  `;
 }
 
 module.exports = {

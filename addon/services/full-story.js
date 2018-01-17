faa3f0d6146e076fc9ee2215312d1384278f659f
David Govea <govea.d@gmail.com>
2018-01-16 16:21:45 -0800

    Defer service method use until injection

--- addon/services/full-story.js
+++ addon/services/full-story.js
@@ -1,20 +1,34 @@
+/* global window */
 import Ember from 'ember';
-import FS from 'full-story';
 
-export default Ember.Service.extend({
+const { Evented } = Ember;
+
+export default Ember.Service.extend(Evented, {
   identify(userId, userVars = {}) {
-    this.inject();
-    FS.identify(userId, userVars);
+    fsMethod('identify', userId, userVars);
   },
 
   setUserVars(userVars = {}) {
-    this.inject();
-    FS.setUserVars(userVars);
+    fsMethod('setUserVars', userVars);
   },
 
   inject() {
-    if (typeof _fs_inject_script === 'function') {
+    if (window && typeof window._fs_inject_script === 'function') {
       _fs_inject_script();
+      this.set('isInjected', true);
+      this.trigger('injected');
     }
-  }
+  },
+
+  isInjected: Ember.computed(function() {
+    return window && window.FS;
+  }),
 });
+
+function fsMethod(method, ...args) {
+  if (window && window.FS) {
+    window.FS[method](...args);
+  } else {
+    this.one('injected', () => fsMethod(...arguments));
+  }
+}
